405ca0fe269c1b484c3a30cbc301becd0926a95e
Cihaan <cihankafadar1@gmail.com>
2024-11-05 17:38:58 +0100

    up

--- packages/auth-service/src/routes/auth.ts
+++ packages/auth-service/src/routes/auth.ts
@@ -75,17 +75,17 @@ router.get(
 router.get(
   '/oauth2/redirect/google',
   passport.authenticate('google', {
-    successRedirect: 'http://localhost:3000/board',
-    failureRedirect: 'http://localhost:3000/',
+    successRedirect: 'http://localhost:5173/panier',
+    failureRedirect: 'http://localhost:5173/',
   })
 );
 
-router.post('/logout', function(req, res, next) {
+router.post('/logout', function (req, res, next) {
   req.logout((err) => {
     if (err) {
       return next(err);
     }
-    res.redirect('http://localhost:3000/');
+    res.redirect('http://localhost:5173/');
   });
 });
 
