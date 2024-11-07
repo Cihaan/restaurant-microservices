b09d1789613c5f0d49f500e6895dfd449abc30c9
Omar <omarhadidi123@gmail.com>
2024-11-07 19:28:07 +0100

    non staged

--- packages/auth-service/drizzle/0000_pink_nekra.sql
+++ packages/auth-service/drizzle/0000_pink_nekra.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS "users" (
 	"last_name" varchar(255) NOT NULL,
 	"username" varchar(255) NOT NULL,
 	"provider" varchar(50) NOT NULL,
+	"role" "user_role" DEFAULT 'client',
 	"provider_id" varchar(255) NOT NULL,
 	"created_at" timestamp DEFAULT now(),
 	"updated_at" timestamp DEFAULT now(),
